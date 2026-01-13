12430a73f5b71f87086f974743dae61c994e73af
cokebarcc <yakupcaglin@hotmail.com>
2026-01-13 22:36:48 +0300

    Configure for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/web-sitesi-projesi/',   // <-- BURAYI EKLE
+  base: '/',   // Netlify için root path
   server: {
     port: 3000,
     host: '0.0.0.0',
